40be26a96a5655d4ccc60fdf547e363e87ad715f
B-Musick <bmuze1@gmail.com>
2024-01-09 09:32:11 -0600

    initialized redux store

---
+++ src/store/slices/plantsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = [
+    { id: nanoid(), genus: 'populous', species: 'tremuloides', family:'populaceae', petals: '2', sepals:'2' },
+    { id: nanoid(), genus: 'betula', species: 'tra', family: 'betulaceae', petals: '3', sepals: '2' },
+]
+
+const plantsSlice = createSlice({
+    name: 'plants',
+    initialState,
+    reducers: {}
+})
+
+export default plantsSlice.reducer
