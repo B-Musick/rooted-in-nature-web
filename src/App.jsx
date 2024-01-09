24a4a3624b419e1affc6a51e1ffbe066cc618ac5
B-Musick <bmuze1@gmail.com>
2024-01-09 13:47:18 -0600

    Added home page, nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
 import PlantsPage from './pages/PlantPage';
 import PlantsList from './components/plants/PlantsList'
 import TopNavbar from './components/TopNavbar';
 
 function App() {
-
   return (
     <>
       {/* Fragment */}
@@ -15,13 +15,14 @@ function App() {
         <Routes>
           {/* Make sure only one route shown at a time */}
           {/* Content inswitch depends on the route */}
-          <Route path="/" element={<PlantsPage />}>
-            <Route path="plants" element={<PlantsPage />}>
-            </Route>
+          <Route path="/">
+            <Route index={true} path="" element={<HomePage />} />
           </Route>
-          <Route path="plants/list" element={<PlantsList />} />
 
-
+          <Route path="/plants">
+            <Route index={true} element={<PlantsPage />} />
+            <Route index={false} path="list" element={<PlantsList />} />
+          </Route>
           {/* One route for each page we want to render */}
           {/* Nest the component we want to show within div when on route */}
         </Routes>
--- src/pages/PlantPage.jsx
+++ src/pages/PlantPage.jsx
@@ -7,7 +7,7 @@ function PlantsPage() {
                 <div className="border-black border-2">Plants</div>
                 <div>
                     <NavLink
-                        to="plants/list"
+                        to="list"
                         className="font-bold border-b-4 border-blue-500 bg-sky-700 w-full text-center"
                     >List</NavLink>
                 </div>
