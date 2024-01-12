bb328b757098e54f795a02a3d25e565f15b5f7ae
B-Musick <bmuze1@gmail.com>
2024-01-12 12:41:14 -0600

    Implemented RTK endpoint to fetch all plants from database.

--- src/components/plants/PlantsList.jsx
+++ src/components/plants/PlantsList.jsx
@@ -1,16 +1,24 @@
-import {useSelector} from 'react-redux';
 import PlantCard from './PlantCard';
+import { useFetchPlantsQuery } from '../../store/apis/plantsApi';
 
 function PlantsList(){
-    const plants = useSelector(state => state.plants);
+    const { data: plants, isLoading, isSuccess, isError  } = useFetchPlantsQuery();
 
-    const renderedPlants = plants.map((plant)=>{
-        return (
-            <div key={plant.id}>
-                <PlantCard plant={plant}/>
-            </div>
-        )
-    });
+    let renderedPlants;
+
+    if(isLoading) {
+        renderedPlants = <div>Loading...</div>
+    } else if (isSuccess) {
+        renderedPlants = plants.map((plant) => {
+            return (
+                <div key={plant.id}>
+                    <PlantCard plant={plant} />
+                </div>
+            )
+        });
+    } else if (isError) {
+        renderedPlants = <div>Error</div>
+    }
 
     return (
         <div>
